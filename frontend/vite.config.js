ae75e0c72fed15b6b971847b6e8cd4e6c8a2e142
carlosalegria@me.com <147549641+CarlosATO@users.noreply.github.com>
2025-11-14 09:32:46 -0300

    Force rebuild to inject environment variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Force rebuild with environment variables
 export default defineConfig({
   plugins: [react()],
   build: {
